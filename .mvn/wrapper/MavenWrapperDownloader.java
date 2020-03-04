881de191d04360ebb8da8012fa254d963d10dc56
LQ <3504566107@qq.com>
2020-03-04 16:06:12 +0800

    完成项目基础配置

--- .mvn/wrapper/MavenWrapperDownloader.java
+++ .mvn/wrapper/MavenWrapperDownloader.java
@@ -32,7 +32,8 @@ public class MavenWrapperDownloader {
      * Path to the maven-wrapper.properties file, which might contain a downloadUrl property to
      * use instead of the default one.
      */
-    private static final String MAVEN_WRAPPER_PROPERTIES_PATH =
+    private static final application.yml
+    application-dev.ymlString MAVEN_WRAPPER_PROPERTIES_PATH =
             ".mvn/wrapper/maven-wrapper.properties";
 
     /**
