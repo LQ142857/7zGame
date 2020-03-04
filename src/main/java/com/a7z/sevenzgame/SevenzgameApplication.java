baf2ea882612cd4923cb82d343f54716278f8fad
LQ <3504566107@qq.com>
2020-03-04 15:42:12 +0800

    创建SpringBoot项目

---
+++ src/main/java/com/a7z/sevenzgame/SevenzgameApplication.java
@@ -0,0 +1,13 @@
+package com.a7z.sevenzgame;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SevenzgameApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SevenzgameApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/a7z/sevenzgame/SevenzgameApplicationTests.java
@@ -0,0 +1,13 @@
+package com.a7z.sevenzgame;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SevenzgameApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
